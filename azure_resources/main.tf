400c25e7742ed420f660aa94a3d69616404d7a4f
Anurag Chauhan <send2anurag.chauhan@gmail.com>
2026-07-01 00:10:23 +0530

    test4

--- azure_resources/main.tf
+++ azure_resources/main.tf
@@ -1,12 +1,32 @@
 resource "azurerm_resource_group" "this"{
     name = "anurag1"
-    location = "central india"
+    location = "Central India"
 }
 resource "azurerm_storage_account" "this1"{
     name = "b18anuragg7"
     resource_group_name = azurerm_resource_group.this.name
-    location = "azurerm_resource_group.this.location"
-    account_tier = "standard"
+    location = azurerm_resource_group.this.location
+    account_tier = "Standard"
     account_replication_type = "LRS"
+    
+    network_rules {
+    default_action = "Deny"
+    bypass = [
+        "AzureServices"
+    ]
+    }
+    
+    min_tls_version      = "TLS1_2"
+    
+    queue_properties  {
+    logging {
+        delete                = true
+        read                  = true
+        write                 = true
+        version               = "1.0"
+        retention_policy_days = 10
+    }
+}
+https_traffic_only_enabled = true
 }
 
